f6c9b9c4d29462f92f7acd532c1b01db99dcca00
Luca Hofman <LucaHofmann@gmx.net>
2015-01-31 13:23:55 +0100

    Added JUnit test.

---
+++ test/com/nukethemoon/ani/tools/test/animation/TestAnimation.java
@@ -0,0 +1,21 @@
+package com.nukethemoon.ani.tools.test.animation;
+
+
+import com.nukethemoon.tools.ani.AbstractAnimation;
+import com.nukethemoon.tools.ani.AnimationFinishedListener;
+
+public class TestAnimation extends AbstractAnimation {
+
+	public TestAnimation(int pDuration, AnimationFinishedListener pListener) {
+		super(pDuration, pListener);
+	}
+
+	@Override
+	protected void onProgress(float pProgress) { }
+
+	@Override
+	protected void onFinish() { }
+
+	@Override
+	protected void onStart() { }
+}
