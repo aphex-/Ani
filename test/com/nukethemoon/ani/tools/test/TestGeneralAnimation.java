8ff395db1ddbd25077ee1b0b5eb3eef614488622
Luca Hofman <LucaHofmann@gmx.net>
2015-02-01 14:29:53 +0100

    Added javadoc.

---
+++ test/com/nukethemoon/ani/tools/test/TestGeneralAnimation.java
@@ -0,0 +1,224 @@
+package com.nukethemoon.ani.tools.test;
+
+import com.nukethemoon.ani.tools.test.animation.TestAnimation;
+import com.nukethemoon.ani.tools.test.animation.TestAnimationLoops;
+import com.nukethemoon.tools.ani.AbstractAnimation;
+import com.nukethemoon.tools.ani.AnimationController;
+import com.nukethemoon.tools.ani.AnimationFinishedListener;
+import org.junit.Test;
+
+import java.util.*;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.Assert.*;
+
+public class TestGeneralAnimation {
+
+	/**
+	 * Tests the duration of an animation with a little tolerance.
+	 * Also tests some lifecycle logic.
+	 * @throws InterruptedException
+	 */
+	@Test
+	public void testAnimationDuration() throws InterruptedException {
+		final CountDownLatch latch = new CountDownLatch(1);
+		int animationDuration = 10;
+
+
+		// you may need to increase the tolerance depending on your CPU performance
+		int differenceTolerance = 5;
+
+		final AnimationController controller = new AnimationController(1);
+
+		// take the start time
+		final List<Long> times = new ArrayList<>();
+		times.add(System.currentTimeMillis());
+
+		TestAnimation testAnimation = new TestAnimation(animationDuration, new AnimationFinishedListener() {
+			@Override
+			public void onAnimationFinished(AbstractAnimation pAnimation) {
+
+				// test if the animation is finished.
+				assertEquals("The animation isFinished method did return false " +
+						"in the animation finished listener.", true, pAnimation.isFinished());
+
+				// take the ending time
+				times.add(System.currentTimeMillis());
+				latch.countDown();
+			}
+		});
+
+		assertEquals("The animation state is 'finished' before it was started.",
+				false, testAnimation.isFinished());
+
+
+		// start the animation
+		controller.addAnimation(testAnimation);
+
+		// wait for the animation to finish
+		latch.await(100, TimeUnit.MILLISECONDS);
+
+		assertEquals("The animation controller still contains the finished animation",
+				0, controller.getAnimationCount());
+
+		// test if the animation listener was called.
+		assertEquals("The animation finished listener was not called within the " +
+				"expected time.", 2, times.size());
+
+		// test of the animation duration is valid.
+		long diff = (times.get(1) - times.get(0)) - animationDuration;
+		assertEquals("The animation ended " + diff + " ms later as expected. The " +
+				"tolerance is " + differenceTolerance  +"ms.", true, diff < 5 && diff >= 0);
+	}
+
+
+	/**
+	 * Tests the progress of an animation with loops.
+	 * Also tests some lifecycle logic.
+	 * @throws InterruptedException
+	 */
+	@Test
+	public void testLoops() throws InterruptedException {
+		AnimationController animationController = new AnimationController(2);
+		final CountDownLatch latch = new CountDownLatch(1);
+
+		int loopCountToTest = 3;
+
+		final List<Float> animationProgressValues = new ArrayList<>();
+		final List<Integer> animationLoopIndexList = new ArrayList<>();
+
+		AnimationFinishedListener listener = new AnimationFinishedListener() {
+			@Override
+			public void onAnimationFinished(AbstractAnimation pAnimation) {
+
+				assertEquals("Animation is still in state 'looping' on finished.",
+						false, pAnimation.isLooping());
+
+				assertEquals("There are still remaining loops on finished.",
+						0, pAnimation.getRemainingLoopCount());
+
+				latch.countDown();
+			}
+		};
+
+		final TestAnimationLoops testAnimation = new TestAnimationLoops(
+				50, animationProgressValues, animationLoopIndexList, listener);
+
+		testAnimation.setLoopLength(loopCountToTest);
+		animationController.addAnimation(testAnimation);
+
+		assertEquals("Animation is not looping after adding to the controller.",
+				true, testAnimation.isLooping());
+
+		latch.await(250, TimeUnit.MILLISECONDS);
+
+		// count 1.0 values
+		int fullProgressCount = Collections.frequency(animationProgressValues, 1.0f);
+		int noProgressCount = Collections.frequency(animationProgressValues, 0.0f);
+
+		assertEquals(true, fullProgressCount >= loopCountToTest + 1);
+		assertEquals(true, noProgressCount >= loopCountToTest);
+
+		// test the call of onLoopStart
+		assertEquals("Expected to call onLoopStart " + loopCountToTest + " times.",
+				loopCountToTest, animationLoopIndexList.size());
+
+		// test the parameter of onLoopStart
+		for (int i = 1; i < animationLoopIndexList.size(); i++) {
+			assertEquals("The call of onLoopStart is not valid.", i, (int) animationLoopIndexList.get(i - 1));
+		}
+	}
+
+	/**
+	 * Tests the usage of pause and resume.
+	 */
+	@Test
+	public void testPauseAndResume() throws InterruptedException {
+
+		int animationDurationToTest = 1000;
+		int pauseTimeToTest = 200;
+
+		// you may need to increase the tolerance depending on your CPU performance
+		int animationDurationTolerance = 20;
+		float progressDifferenceTolerance = 0.008f;
+
+		AnimationController controller = new AnimationController(3);
+		Timer timer = new Timer();
+
+		final CountDownLatch animationLatch = new CountDownLatch(1);
+		final List<Long> times = new ArrayList<>();
+
+		times.add(System.currentTimeMillis());
+
+		TestAnimation animation = new TestAnimation(animationDurationToTest,
+				new AnimationFinishedListener() {
+			@Override
+			public void onAnimationFinished(AbstractAnimation pAnimation) {
+				times.add(System.currentTimeMillis());
+				animationLatch.countDown();
+			}
+		});
+
+		controller.addAnimation(animation);
+		wait(pauseTimeToTest, timer); // animation is running
+
+		assertProgress(animation, pauseTimeToTest,
+				animationDurationToTest, progressDifferenceTolerance);
+
+		animation.pause();
+
+		assertEquals("Animation is not paused.", true, animation.isPaused());
+		wait(pauseTimeToTest, timer); // animation is paused
+
+
+		assertProgress(animation, pauseTimeToTest,
+				animationDurationToTest, progressDifferenceTolerance);
+
+		animation.resume();
+		assertEquals("Animation is paused.", false, animation.isPaused());
+
+		assertProgress(animation, pauseTimeToTest,
+				animationDurationToTest, progressDifferenceTolerance);
+		assertEquals("Animation is finished.", false, animation.isFinished());
+
+		animationLatch.await(animationDurationToTest * 2, TimeUnit.MILLISECONDS);
+
+		long animationTimeMeasured = times.get(1) - times.get(0);
+		long animationTimeExpected = animationDurationToTest + pauseTimeToTest;
+		long animationTimeDifference = animationTimeMeasured - animationTimeExpected;
+
+		assertEquals("Animation duration was " + animationTimeMeasured + "ms " +
+				"but expected was " + animationTimeExpected + "ms by a tolerance of " +
+				animationDurationTolerance,
+				true,
+				animationTimeDifference < animationDurationTolerance);
+
+		assertEquals("Animation is not finished. ", true, animation.isFinished());
+
+	}
+
+	private void assertProgress(AbstractAnimation pAnimation, float pauseTime,
+								float pAnimationDuration, float pTolerance) {
+		float currentProgress = pAnimation.update();
+		float expectedProgress = (pauseTime / pAnimationDuration);
+		float progressDifference = Math.abs(currentProgress - expectedProgress);
+		assertEquals("The expected progress is " + expectedProgress + " but the current" +
+						"progress is " + currentProgress + " by a tolerance of " + pTolerance,
+				true,
+				progressDifference < pTolerance
+		);
+	}
+
+	public void wait(int pMillis, Timer pTimer) throws InterruptedException {
+		final CountDownLatch pauseLatch = new CountDownLatch(1);
+		TimerTask timerTask = new TimerTask() {
+			@Override
+			public void run() {
+				pauseLatch.countDown();
+			}
+		};
+		pTimer.schedule(timerTask, pMillis);
+		pauseLatch.await(pMillis * 2, TimeUnit.MILLISECONDS);
+	}
+}
