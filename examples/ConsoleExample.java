41864d7d58094dc022d6d1dcae40d21aa004e05f
Luca Hofman <LucaHofmann@gmx.net>
2015-02-01 15:33:51 +0100

    Added console example.

--- examples/ConsoleExample.java
+++ examples/ConsoleExample.java
@@ -1,5 +1,3 @@
-package com.nukethemoon.tools.ani;
-
 import com.nukethemoon.tools.ani.AbstractAnimation;
 import com.nukethemoon.tools.ani.AnimationController;
 import com.nukethemoon.tools.ani.AnimationFinishedListener;
@@ -7,20 +5,17 @@ import com.nukethemoon.tools.ani.AnimationFinishedListener;
 import java.lang.Override;
 
 /**
- * @author luca.hofmann
+ * Example to print on the console.
  */
 public class ConsoleExample {
 
 	/**
-	 * An animation that inherits form AbstractAnimation and animates "Hello World" to the console.
+	 * Animates "Hello World" to the console.
 	 */
 	public static class HelloWorldAnimation extends AbstractAnimation{
 
 		private String textToPrint;
 
-		/**
-		 * The Hello World animation.
-		 */
 		public HelloWorldAnimation(AnimationFinishedListener pAnimationFinishedListener) {
 			// set the duration of the animation to its parent
 			super(1000, pAnimationFinishedListener);
@@ -28,10 +23,6 @@ public class ConsoleExample {
 			setLoopLength(1);
 		}
 
-		/**
-		 * Implement what should happen while animating.
-		 * @param pProgress The progress of the animation.
-		 */
 		@Override
 		public void onProgress(float pProgress) {
 			// print the text using the progress as length.
@@ -59,20 +50,14 @@ public class ConsoleExample {
 	}
 
 	public static void main(String[ ] args) {
-
 		AnimationController animationController = new AnimationController(10);
-
 		HelloWorldAnimation myAnimation = new HelloWorldAnimation(new AnimationFinishedListener() {
 			@Override
 			public void onAnimationFinished(AbstractAnimation pAnimation) {
 				System.out.println("onAnimationFinished()");
 			}
 		});
-
 		// add the animation
 		animationController.addAnimation(myAnimation);
 	}
-
-
-
 }
